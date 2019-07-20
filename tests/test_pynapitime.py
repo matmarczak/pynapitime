4f8f2188657375d4a9a5ee67961ee77e855ddb79
Mateusz Marczak <amonowy@gmail.com>
2019-07-20 12:30:19 +0200

    test: add tests fo pynapitime main script

---
+++ tests/test_pynapitime.py
@@ -0,0 +1,25 @@
+from tests.factories import movie_file
+from pynapitime import handle_file
+import unittest
+from unittest.mock import patch
+from utils.exceptions import BadFile
+from tests.test_video import file_mocker
+
+class TestPynapitime(unittest.TestCase):
+    @classmethod
+    def setUp(cls):
+        cls.temp_movie = movie_file()
+        cls.temp_movie.touch()
+
+    def test_handle_file_raises_exc_on_bad_file(self):
+        with self.assertRaises(BadFile):
+            handle_file(self.temp_movie)
+
+    @file_mocker
+    def test_handle_file(self):
+        # TODO
+        # Mocks video duration to allow program handling movie subtitiles
+        # from Browser, read about unittest.mock to mock duration of video
+        with patch('pynapitime.Video.duration') as mock:
+            mock.side_effect = 123
+            handle_file(self.temp_movie)
