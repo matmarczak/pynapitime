cde536be973c404daed7b1c05f1b27bfbd96ab8f
Mateusz Marczak <mateusz.marczak@10clouds.com>
2019-12-17 20:11:11 +0100

    Refactor video tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,9 @@
+from typing import NamedTuple
+from unittest.mock import Mock, patch
+
 import pytest
+
+from utils.video import Video
 
 series_episodes = [
     "Friends.S01E09.1994.super.mkv",
@@ -6,6 +11,47 @@ series_episodes = [
     "Friends.S01E08.1994.super.mkv",
 ]
 
+
 @pytest.fixture(params=series_episodes)
 def series_episode(request):
     return request.param
+
+
+class TestMovie(NamedTuple):
+    filename: str
+    title: str
+    year: int
+
+
+test_movies = [
+    TestMovie(
+        "Jumanji.Welcome.to.the.Jungle.2017.480p.BluRay.x264.mkv",
+        "Jumanji Welcome to the Jungle",
+        2017
+    ),
+    TestMovie(
+        "Jumanji.Welcome.to.the.Jungle.480p.BluRay.x264.mkv",
+        "Jumanji Welcome to the Jungle",
+        None
+    )
+]
+@pytest.fixture(params=test_movies)
+def movie(request):
+    return request.param
+
+
+@pytest.fixture
+def movie_path(movie, tmpdir):
+    return tmpdir.join(movie.filename)
+
+
+@pytest.fixture
+def video(movie_path):
+    return Video(movie_path)
+
+
+@pytest.fixture
+def track_data():
+    mock_extract = Mock(return_value=(2134, "24"))
+    with patch("utils.video.Video._extract_video_track", mock_extract):
+        yield
--- tests/test_video.py
+++ tests/test_video.py
@@ -1,81 +1,5 @@
 import pathlib
-from unittest import TestCase
-
-from .factories import movie_file, file_mocker, TEST_MOVIES
 from utils.video import Video
-
-
-class VideoTest:
-    @classmethod
-    def setUpClass(cls):
-        cls.temp_movie = movie_file(cls.test_movie_file)
-        cls.video = Video(cls.temp_movie)
-
-    def test_video_saves_path(self):
-        self.assertIsInstance(self.video.path, pathlib.Path)
-        self.assertEqual(self.video.path.__str__(), str(self.temp_movie))
-
-    def test_check_for_subs_doesnt_exists(self):
-        subs_path = self.video.path.with_suffix(".mkv.txt")
-        if subs_path.exists():
-            # ensure path is clean
-            subs_path.unlink()
-        self.video.subs_exist()
-        self.assertFalse(self.video.subtitles_exist)
-
-    def test_check_if_existing_subtitles_are_handles(self):
-        subs_path = self.video.path.with_suffix(".mkv.txt")
-        subs_path.touch()
-        with subs_path.open("w") as file:
-            file.write("tests subs")
-        self.assertEqual(subs_path, self.video.path.with_suffix(".mkv.txt"))
-        self.assertTrue(subs_path.exists())
-        self.video.subs_exist()
-        self.assertTrue(self.video.subtitles_exist)
-
-    @file_mocker
-    def test_get_track_data(self):
-        self.video.get_track_data()
-        self.assertIsInstance(self.video.duration, int)
-        self.assertIsInstance(self.video.frame_rate, str)
-
-    @file_mocker
-    def test_results_tupes(self):
-        self.video.collect_movie_data()
-        self.assertIsInstance(self.video.duration, int)
-        self.assertIsInstance(self.video.frame_rate, str)
-        self.assertIsInstance(self.video.title, str)
-        self.assertIsInstance(self.video.year, (int, type(None)))
-
-
-class TestVideo_1(VideoTest, TestCase):
-    test_movie_file = TEST_MOVIES[0]
-
-    def test_get_name(self):
-        self.video.parse_name()
-        self.assertEqual(self.video.title, "Jumanji Welcome to the Jungle")
-        self.assertEqual(self.video.year, 2017)
-
-    @file_mocker
-    def test_gather_data(self):
-        self.video.collect_movie_data()
-        self.assertEqual(self.video.duration, 2134)
-        self.assertEqual(self.video.frame_rate, "24")
-
-
-class TestVideo_2(VideoTest, TestCase):
-    test_movie_file = TEST_MOVIES[1]
-
-    def test_get_name(self):
-        self.video.parse_name()
-        self.assertEqual(self.video.title, "Jumanji Welcome to the Jungle")
-        self.assertEqual(self.video.year, None)
-
-    @file_mocker
-    def test_gather_data(self):
-        self.video.collect_movie_data()
-        self.assertEqual(self.video.duration, 2134)
-        self.assertEqual(self.video.frame_rate, "24")
 
 
 class TestSeriesEpisodes:
@@ -86,5 +10,49 @@ class TestSeriesEpisodes:
         assert test_video.year
         assert test_video.episode
         assert test_video.season
-        assert test_video.episode == "09"
-        assert test_video.season == "01"
+
+
+class TestVideo:
+    def test_video_saves_path(self, video, movie_path):
+        assert isinstance(video.path, pathlib.Path)
+        assert video.path.__str__() == str(movie_path)
+
+    def test_check_for_subs_doesnt_exists(self, video):
+        subs_path = video.path.with_suffix(".mkv.txt")
+        if subs_path.exists():
+            # ensure path is clean
+            subs_path.unlink()
+        video.subs_exist()
+        assert not video.subtitles_exist
+
+    def test_check_if_existing_subtitles_are_handles(self, video):
+        subs_path = video.path.with_suffix(".mkv.txt")
+        subs_path.touch()
+        with subs_path.open("w") as file:
+            file.write("tests subs")
+        assert subs_path == video.path.with_suffix(".mkv.txt")
+        assert subs_path.exists()
+        assert video.subs_exist()
+        assert video.subtitles_exist
+
+    def test_get_track_data(self, track_data, video):
+        video.get_track_data()
+        assert isinstance(video.duration, int)
+        assert isinstance(video.frame_rate, str)
+
+    def test_results_tuples(self, video, track_data):
+        video.collect_movie_data()
+        isinstance(video.duration, int)
+        isinstance(video.frame_rate, str)
+        isinstance(video.title, str)
+        isinstance(video.year, (int, type(None)))
+
+    def test_get_name(self, video, movie):
+        video.parse_name()
+        assert video.title == movie.title
+        assert video.year == movie.year
+
+    def test_gather_data(self, video, track_data):
+        video.collect_movie_data()
+        assert video.duration == 2134
+        assert video.frame_rate == "24"
